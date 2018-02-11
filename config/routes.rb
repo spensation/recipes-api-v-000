016739af992246feb4873747e696a31b35a079be
Spencer Evans <github email address>
2018-02-10 19:07:03 -0800

    API routes namespaced

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :recipes
+  namespace :api do
+    namespace :v1 do
+      resources :recipes, only: [:index, :show, :create, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    end
+  end 
 end
