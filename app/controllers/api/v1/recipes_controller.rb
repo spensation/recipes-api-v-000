3530e1c57456b08f1ade64f4b5bb453a542f17f1
Spencer Evans <github email address>
2018-02-14 18:16:16 -0800

    added destroy method to recipesController

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -4,9 +4,28 @@ class Api::V1::RecipesController < ApplicationController
     render json: @recipes
   end
 
+  def create
+    @recipe = Recipe.create(recipe_params)
+    render json: @recipe
+  end
+
   def show
     @recipe = Recipe.find(params[:id])
     render json: @recipe.to_json
   end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    if @recipe.destroy
+        render json: :no_content, status: 202
+    else
+      render json: {}, status: 400
+    end
+  end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:title, :category, :serves, :prep_time, :cook_time, :total_time, :ingredients, :directions)
+    end
 
 end
